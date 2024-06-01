2b8a94352ec8757fc89114f286fcd9014ae2c51a
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-06-01 10:47:21 +0200

    chore: get btc price from coingecko

--- arbnode/seq_coordinator.go
+++ arbnode/seq_coordinator.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"sync"
 	"sync/atomic"
@@ -856,4 +857,36 @@ func (c *SeqCoordinator) SeekLockout(ctx context.Context) {
 			log.Warn("failed to set wants lockout key in redis after seeking lockout again", "err", err)
 		}
 	}
+}
+
+type HttpClient interface {
+	Get(url string) (*http.Response, error)
+}
+
+type BtcUsdPrice struct {
+	Bitcoin struct {
+		Usd int `json:"usd"`
+	} `json:"bitcoin"`
+}
+
+// getBtcUsdPrice gets the price of Bitcoin from Coingecko
+// simplify assumptioin that it will return ints
+func getBtcUsdPrice(client HttpClient) (int, error) {
+	resp, err := client.Get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	var price BtcUsdPrice
+	if err := json.Unmarshal(body, &price); err != nil {
+		return 0, err
+	}
+
+	return price.Bitcoin.Usd, nil
 }
--- arbnode/seq_coordinator_atomic_test.go
+++ arbnode/seq_coordinator_atomic_test.go
@@ -4,9 +4,13 @@
 package arbnode
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"math/rand"
+	"net/http"
+	"net/http/httptest"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -155,4 +159,45 @@ func TestRedisSeqCoordinatorAtomic(t *testing.T) {
 		time.Sleep(time.Millisecond * 20)
 	}
 
+}
+
+type MockHttpClient struct {
+	MockResponse *http.Response
+	MockError    error
+}
+
+func (m *MockHttpClient) Get(url string) (*http.Response, error) {
+	return m.MockResponse, m.MockError
+}
+
+func TestGetBtcUsdPrice(t *testing.T) {
+	// Create a mock response
+	mockPrice := map[string]map[string]int{
+		"bitcoin": {
+			"usd": 68513,
+		},
+	}
+	body, _ := json.Marshal(mockPrice)
+
+	// Use httptest to create a response recorder
+	recorder := httptest.NewRecorder()
+	recorder.Body = bytes.NewBuffer(body)
+	recorder.Header().Set("Content-Type", "application/json")
+	recorder.WriteHeader(http.StatusOK)
+
+	client := &MockHttpClient{
+		MockResponse: recorder.Result(),
+		MockError:    nil,
+	}
+
+	// Test the getBtcUsdPrice function
+	price, err := getBtcUsdPrice(client)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	expectedPrice := 68513
+	if price != expectedPrice {
+		t.Errorf("expected price %d, got %d", expectedPrice, price)
+	}
 }
