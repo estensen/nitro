dd5f698f0b5d2b7da76cb1b17aaeaebb61ead6ac
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-06-05 21:25:55 +0200

    chore: test price feed precompile

--- precompiles/ArbPriceFeed_test.go
+++ precompiles/ArbPriceFeed_test.go
@@ -1,1 +1,32 @@
 package precompiles
+
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func TestPriceFeed(t *testing.T) {
+	evm := newMockEVMForTesting()
+	feed := ArbPriceFeed{}
+
+	callerCtx := testContext(common.Address{}, evm)
+
+	// Initial result should be zero
+	price, err := feed.GetLatestBtcPrice(callerCtx, evm)
+	Require(t, err)
+	if price != 0 {
+		t.Errorf("price is not zero, got: %d, wanted: 0", price)
+	}
+
+	// Set price
+	err = feed.SetLatestBtcPrice(callerCtx, evm, 70000)
+	Require(t, err)
+
+	// Should be able to get the price that was set
+	price, err = feed.GetLatestBtcPrice(callerCtx, evm)
+	Require(t, err)
+	if price != 70000 {
+		t.Errorf("price is not equal, got: %d, wanted: %d", price, 70000)
+	}
+}
